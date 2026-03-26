522e1b8a21418ffa9227433619c684b61870b224
Ali <topolovskoda@gmail.com>
2026-03-26 15:02:28 +0100

    next.config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: ["@prisma/client", "pg"],
+
+  turbopack: {
+    resolveAlias: {
+      // Helps Turbopack find your custom Prisma output
+      "../generated/prisma": "./generated/prisma",
+      // or if using @ alias:
+      // "@/generated/prisma": "./generated/prisma",
+    },
+  },
 };
 
 export default nextConfig;
